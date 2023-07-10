34eef97ebaab029a281a14ff6da4692aecb65a5c
RashidMur <rashidm1990@mail.ru>
2023-07-10 14:55:52 +0300

    fulfillment of the conditions of step 6

---
+++ app/src/test/java/hexlet/code/DifferTests.java
@@ -0,0 +1,60 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class DifferTests {
+    @Test
+    public void testGenerate() throws Exception {
+        var filepath1 = "src/test/resources/file1.json";
+        var filepath2 = "src/test/resources/file2.json";
+
+        var expected = "\n" + "{\n"
+                + "  - follow: false\n"
+                + "    host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "  + timeout: 20\n"
+                + "  + verbose: true\n"
+                + "}";
+        var actual = Differ.generate(filepath1, filepath2);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIfExists() {
+        var filepath1 = "src/test/resources/file3.json";
+        var filepath2 = "src/test/resources/file2.json";
+
+        var thrown = catchThrowable(
+                () -> Differ.generate(filepath1, filepath2)
+        );
+        assertThat(thrown).isInstanceOf(Exception.class);
+    }
+
+    @Test
+    public void testEmptyFile() throws Exception {
+        var filepath1 = "src/test/resources/file1.json";
+        var filepath2 = "src/test/resources/fileEmpty.json";
+
+        var thrown = catchThrowable(
+                () -> Differ.generate(filepath1, filepath2)
+        );
+        assertThat(thrown).isInstanceOf(Exception.class);
+    }
+
+    @Test
+    public void wrongFilepath() throws Exception {
+        var filepath1 = "src/test/resources/file1.json";
+        var filepath2 = "file2";
+
+        var thrown = catchThrowable(
+                () -> Differ.generate(filepath1, filepath2)
+        );
+        assertThat(thrown).isInstanceOf(Exception.class);
+    }
+}
