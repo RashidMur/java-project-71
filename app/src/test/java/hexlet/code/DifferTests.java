7d82e63d0c558640fa791611f06af869e03dad1d
RashidMur <rashidm1990@mail.ru>
2023-07-19 13:56:07 +0300

    create fileNumb1 and 2 and DifferTests

--- app/src/test/java/hexlet/code/DifferTests.java
+++ app/src/test/java/hexlet/code/DifferTests.java
@@ -13,12 +13,25 @@ public class DifferTests {
         var filepath2 = "src/test/resources/fileNumb2.json";
 
         var expected = "\n" + "{\n"
-                + "  - follow: false\n"
-                + "    host: hexlet.io\n"
-                + "  - proxy: 123.234.53.22\n"
-                + "  - timeout: 50\n"
-                + "  + timeout: 20\n"
-                + "  + verbose: true\n"
+                + "   chars1: [a, b, c]\n"
+                + " - chars2: [d, e, f]\n"
+                + " + chars2: false\n"
+                + " - checked: false\n"
+                + " + checked: true\n"
+                + " - key1: value1\n"
+                + " + key2: value2\n"
+                + "   numbers1: [1, 2, 3, 4]\n"
+                + " - numbers2: [2, 3, 4, 5]\n"
+                + " + numbers2: [22, 33, 44, 55]\n"
+                + " - numbers3: [3, 4, 5]\n"
+                + " + numbers4: [4, 5, 6]\n"
+                + " + obj1: {nestedKey=value, isNested=true}\n"
+                + " - setting1: Some value\n"
+                + " + setting1: Another value\n"
+                + " - setting2: 200\n"
+                + " + setting2: 300\n"
+                + " - setting3: true\n"
+                + " + setting3: none\n"
                 + "}";
         var actual = Differ.generate(filepath1, filepath2);
 
